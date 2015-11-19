c258fc923d0779883b311ef24d6d399808968208
Markham Anderson <quinesquines@gmail.com>
2015-11-19 12:59:14 -0800

    Initial commit

---
+++ test.sh
@@ -0,0 +1,18 @@
+#!/bin/sh 
+
+SOLUTION=hw5.pl   # only need to configure this if necessary 
+QUERY='ty,halt'
+REFOUT=Output.correct 
+TMPOUT=Output.your
+
+# cleanup 
+echo "(1) remove existing output"
+rm -f $TMPOUT
+
+# run 
+echo "(2) consult your solution and query" 
+gprolog --consult-file part1.facts.pl --consult-file $SOLUTION --consult-file test.pl --query-goal $QUERY >& /dev/null
+
+# diff 
+echo "(3) check for differences" 
+diff $TMPOUT $REFOUT
