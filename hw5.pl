d662baf7c46cfce9886402e1f4a5d23744871ef5
Markham Anderson <quinesquines@gmail.com>
2015-12-01 11:23:57 -0800

    Implement farmer-wolf-goat-cabbage puzzle

--- hw5.pl
+++ hw5.pl
@@ -1,3 +1,69 @@
+/*
+	Working on the farmer, wolf, goat, cabbage ferrying across a river puzzle
+*/
+
+/*
+	Solve should check if move from state(l,l,l,l) to state(r,r,r,r) with history [] works
+*/
+
+%% 
+opposite(left, right).
+opposite(right, left).
+
+%% Initial state is true
+state(left, left, left, left).
+%% Any state which can be reached from another state is true
+state(F, W, G, C) :- move(state(F0, W0, G0, C0), state(F, W, G, C), []).
+
+print_history([]).
+print_history([arc(take(Obj, B0, B1), _, State1)|T]) :-
+	print_history(T),
+	print('take '), print(Obj), print(' from '), print(B0), print(' to '), print(B1), nl,
+	print('\t'), print(State1), nl.
+
+%% A move succeeds if
+%% - its initial state is a legal state
+%% - its terminal state is a legal state
+%% - its proposed take is not a member of its history	.
+move(State0, State1, History) :-
+	safe(State0),
+	A = arc(_, State0, State1),
+	A,
+	\+ member(A, History),
+	print_history([A|History]),
+	!.
+move(State0, State1, History) :-
+	safe(State0),
+	Arc0 = arc(take(_,_,_), State0, StateIntermediate),
+	\+ member(Arc0, History),
+	Arc0,
+	move(StateIntermediate, State1, [Arc0|History]).
+
+safe(state(F, _, G, _)) :- F = G, !.
+safe(state(_, W, G, C)) :- opposite(W,G), opposite(G,C).
+unsafe(A) :- \+ safe(A).
+
+take(Obj, A, B) :- opposite(A, B).
+
+arc(take(wolf, F0, F1), state(F0, F0, G, C), state(F1, F1, G, C)) :-
+	take(wolf, F0, F1),
+	safe(state(F1, F1, G, C)).
+arc(take(goat, F0, F1), state(F0, W, F0, C), state(F1, W, F1, C)) :-
+	take(goat, F0, F1),
+	safe(state(F1, W, F1, C)).
+arc(take(cabbage, F0, F1), state(F0, W, G, F0), state(F1, W, G, F1)) :-
+	take(cabbage, F0, F1),
+	safe(state(F1, W, G, F1)).
+arc(take(farmer, F0, F1), state(F0, W, G, C), state(F1, W, G, C)) :-
+	take(farmer, F0, F1),
+	safe(state(F1, W, G, C)).
+
+solve :- move(state(left, left, left, left), state(right, right, right, right), []), !.
+
+/*
+	Simple exercises
+*/
+
 %% Find all courses with 3 or 4 credits (fc course).
 fc_course(X) :-
 	course(X,_,N),
